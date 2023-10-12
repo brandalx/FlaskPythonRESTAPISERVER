70f29c82d7feb995577dfd6bd7b8ccc06ad9b75f
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 03:21:42 +0400

    feat: user model ready

--- website/models.py
+++ website/models.py
@@ -4,3 +4,6 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
