ddb52b3cbcacf0ec45369316085e17517c0abad6
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-12 21:47:12 +0400

    feat: auth basic routes init

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,18 @@
 from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "<p>Login init</p>"
+
+
+@auth.route("/logout")
+def logout():
+    return "<p>Logout init</p>"
+
+
+@auth.route("/signup")
+def signup():
+    return "<p>Signup init</p>"
