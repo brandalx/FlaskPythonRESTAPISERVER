bc266660fedc48a7328f646e81de1aa03ad18fd0
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-12 18:00:34 +0400

    feat: env conf

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()  
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
