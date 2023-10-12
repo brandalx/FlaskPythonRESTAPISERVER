396575f81555bffb228e80c2856a935cdfd1bb4b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-12 21:37:18 +0400

    feat: basics routes init

--- website/__init__.py
+++ website/__init__.py
@@ -2,10 +2,17 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-load_dotenv()  
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_perfix="/")
+    app.register_blueprint(auth, url_perfix="/")
+
     return app
- 
