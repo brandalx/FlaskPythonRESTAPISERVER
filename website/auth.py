565d195b2ab526e44c32a217a07959793e8b920b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-12 23:26:22 +0400

    feat: sign up form template

--- website/auth.py
+++ website/auth.py
@@ -16,3 +16,11 @@ def logout():
 @auth.route("/signup")
 def signup():
     return render_template("signup.html")
+
+
+# for testing purposes
+# passing any vars
+
+# @auth.route("/login")
+# def login():
+#     return render_template("login.html", text="testing", user="tim")
