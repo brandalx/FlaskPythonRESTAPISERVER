9017920955e9e837ea3b9fc5d24f2fee12e0b388
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 03:18:47 +0400

    feat: database creation and initial db model

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,19 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite: ///{DB_NAME}"
+    db.init_app(app)
     from .views import views
     from .auth import auth
 
