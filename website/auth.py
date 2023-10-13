46658a93e55a6ebf016d00d92111a0b6ab9ef44a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 05:05:20 +0400

    feat: loggin in correctly

--- website/auth.py
+++ website/auth.py
@@ -9,8 +9,20 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    data = request.form
-    print(data)
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+            else:
+                flash("Incorrect password, try again", category="error")
+
+        else:
+            flash("Email does not exist", category="error")
     return render_template("login.html")
 
 
@@ -26,6 +38,11 @@ def signup():
         firstName = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists", category="error")
+            return render_template("signup.html")
 
         if len(email) < 4:
             flash("Email must be greater than 4 characters", category="error")
