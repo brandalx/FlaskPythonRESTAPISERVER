444f4e4f0c19c04f59877512e83fa0beaa29acee
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 06:17:37 +0400

    feat: note deletion route works

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
+import json
 from . import db
 from .models import Note
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 views = Blueprint("views", __name__)
 
@@ -22,3 +23,16 @@ def home():
             flash("Note added!", category="success")
 
     return render_template("home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note["noteId"]
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+            return jsonify({})
