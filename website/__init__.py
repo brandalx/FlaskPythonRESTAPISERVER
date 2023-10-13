9151cabdbcd52dacf9004e01166304c2e83e6845
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 04:28:26 +0400

    feat: server works correctly with sql db

--- website/__init__.py
+++ website/__init__.py
@@ -7,19 +7,22 @@ load_dotenv()
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+PROTECTED_STRING = "sqlite:///"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite: ///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'{PROTECTED_STRING}{DB_NAME}'
+    print(f'{PROTECTED_STRING}{DB_NAME}')
+
     db.init_app(app)
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_perfix="/")
-    app.register_blueprint(auth, url_perfix="/")
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, Note
 
@@ -30,5 +33,6 @@ def create_app():
 
 def create_database(app):
     if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
+        with app.app_context():
+            db.create_all()
         print("Created Database!")
