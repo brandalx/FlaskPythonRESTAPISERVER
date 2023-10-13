c62f27c16553c338c66dbb56c0b77b299a2e4e2d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 05:10:58 +0400

    feat: login logout from flask login

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,17 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=["GET", "POST"])
+@login_required
 def login():
+    login_user()
+    return redirect(url_for("auth.login"))
 
     if request.method == "POST":
         email = request.form.get("email")
@@ -18,6 +22,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in successfully!", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for("views.home"))
             else:
                 flash("Incorrect password, try again", category="error")
@@ -61,6 +66,7 @@ def signup():
 
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account created successfully", category="success")
             return redirect(url_for("views.home"))
 
